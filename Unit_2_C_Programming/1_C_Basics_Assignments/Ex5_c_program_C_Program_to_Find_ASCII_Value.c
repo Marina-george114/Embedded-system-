04b100d99c98da4a8dab5f76d06b63fa739fc5ee
marina_george <marinago01012@gmail.com>
2025-07-26 21:45:12 +0300

    add Ex5

---
+++ Unit_2_C_Programming/1_C_Basics_Assignments/Ex5_c_program_C_Program_to_Find_ASCII_Value.c
@@ -0,0 +1,16 @@
+/*
+ * Ex5_c_program_C_Program_to_Find_ASCII_Value.c
+ *
+ *  Created on: Jul 26, 2025
+ *      Author: Marina
+ */
+
+
+#include <stdio.h>
+int main (){
+	char x;
+	printf("enter a character : ");
+	scanf("%c",&x);
+	printf("ASCII value of %c:%d ",x,x);
+	return 0;
+}
